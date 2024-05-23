78a8285088173631da6e93304e7ce7647e083dfe
kaiquepintor <kaiquepintor.kp@gmail.com>
2024-05-23 13:40:43 -0300

    Co-authored-by: Felipe Barbour <barbourdev@users.noreply.github.com>

---
+++ js/login.js
@@ -0,0 +1,4 @@
+let listaUsuarios = [
+    {nomeCompleto: "José das Couve",emailUsuario:"jo@email.com",senhaUsuario:"123456"},
+    {nomeCompleto: "Joaquim Neves",emailUsuario:"ju@email.com",senhaUsuario:"123456"}
+  ];
